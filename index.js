e848f0a85b6fabc604e7932e10015a576ba422c2
Paul <chorkunxin@gmail.com>
2025-05-29 18:18:28 +0800

    Added code to insert

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mysql2 = require("mysql2/promise");
 const ejs = require("ejs");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+}
+
+const dbConnection = mysql2.createPool(dbConfig);
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -20,8 +32,10 @@ app.get('/food-entry/add', async (req, res) => {
 
 app.post('/food-entry/add', async (req, res) => {
     const { dateTime, foodName, calories, meal, tags, servingSize, unit } = req.body;
-    console.log(dateTime, foodName, calories, meal, tags, servingSize, unit);
-    res.send("Food entry added successfully");
+    const query = "INSERT INTO food_entries (dateTime, foodName, calories, meal, tags, servingSize, unit) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const values = [dateTime, foodName, calories, meal, tags, servingSize, unit];
+    await dbConnection.execute(query, values);
+    res.redirect("/");
 })
 
 app.listen(port, () => {
